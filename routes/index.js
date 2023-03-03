621e9c83370c6a66e15e4261e01828316fd1a2b7
Josep Valls <alfredvays@gmail.com>
2023-03-03 17:21:27 +0100

    Añado connection en routes/index
     Changes to be committed:
    	modified:   lib/mongoClient.js
    	modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const createError = require('http-errors');
+const connection = require('../lib/mongoClient');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
-});
+const init = async () => {
+  const db = await connection(); // obtenemos la conexión
+
+  console.log(`Connected to DB in ${db.databaseName}DB`)
+
+  /* GET home page. */
+  router.get('/', async function (req, res, next) {
+    try {
+      const response = await db.collection('contracts').find({}).toArray();
+      res.json({ response });
+    } catch (error) {
+      next(createError(500, "Contracts are not available in this moment"));
+    }
+  });
+};
+
+init();
 
 module.exports = router;
