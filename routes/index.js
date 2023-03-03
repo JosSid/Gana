b0113da062905a60d3063e3f5887f6b541a00e90
Josep Valls <alfredvays@gmail.com>
2023-03-04 00:11:17 +0100

    Funcionando rutas /getlocalidad y /listcontracts  Changes to be committed: 	new file:   lib/readFile.js 	modified:   package-lock.json 	modified:   package.json 	new file:   public/excel/cp-municipios.xlsx 	modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,35 @@ const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
 const connection = require('../lib/mongoClient');
+const fileData = require('../lib/readFile');
 
 const init = async () => {
   const db = await connection(); // obtenemos la conexión
 
-  console.log(`Connected to DB in ${db.databaseName}DB`)
+  console.log(`Connected to DB in ${db.databaseName}DB`);
 
   /* GET home page. */
-  router.get('/', async function (req, res, next) {
+  router.get('/listcontracts', async function (req, res, next) {
     try {
       const response = await db.collection('contracts').find({}).toArray();
-      res.json({ response });
+      res.status(200).json({ response });
     } catch (error) {
-      next(createError(500, "Contracts are not available in this moment"));
+      next(createError(500, 'Contracts are not available in this moment'));
     }
+  });
+
+  router.get('/getlocalidad/:cp', async function (req, res, next) {
+    const cp = req.params.cp;
+
+    const localidades = [];
+
+    for (let localidad of fileData) {
+      if (localidad.codigo_postal.includes(cp)) {
+        localidades.push(localidad);
+      }
+    }
+
+    res.status(200).json(localidades);
   });
 };
 
